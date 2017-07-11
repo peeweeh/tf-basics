1c0dcb3bf28d981941c780e695a2a7c044ff250c
Paul Hidalgo <me@paulhidalgo.net>
2017-07-11 11:15:36 +0800

    Cleaning up

---
+++ aws/iam/cross-account/provider_account.tf
@@ -0,0 +1,22 @@
+# create a group, which will be able to assume "ExternalAdminRole" from dev account
+resource "aws_iam_group" "provider_iam_group" {
+    provider = "aws.provider"
+    name = "${var.managed_name}-${var.provider_name}-group"
+}
+
+# create a group policy, which allows to assume "ExternalAdminRole"
+resource "aws_iam_group_policy" "provider_iam_policy" {
+    provider = "aws.provider"
+    name = "${var.managed_name}-${var.provider_name}-policy"
+    group = "${aws_iam_group.provider_iam_group.id}"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": {
+        "Effect": "Allow",
+        "Action": "sts:AssumeRole",
+        "Resource": "${aws_iam_role.external_admin_role.arn}"
+    }
+}
+EOF
+}
